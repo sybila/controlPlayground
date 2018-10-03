2c02d6b7f9ef12d9be1441383b736a088d90bee9
Matej Trojak <trojak@mail.muni.cz>
2018-10-03 12:59:01 +0200

    Improved scripts

--- libs/scm_scripts/set_values.scm
+++ libs/scm_scripts/set_values.scm
@@ -1,3 +1,8 @@
 (define (main arguments)
   (for-each print (cdr arguments))
   0)
+
+;  (rpc2 GMS01 `(set-valve-tflow 0 5) ; prvni parametr 0 je CO2
+;  (rpc2 GMS01 `(set-valve-tflow 1 495) ; druhý parametr 1 je vzduch = 1% CO2 ve vzduchu pro takto nastavene
+; vysledna koncentrace v % je definovana jako (flowCO2 / (flowCO2 + flowAir) + 400 / 1e6) * 100 || v ppm flowCO2 / (flowCO2 + flowAir) * 1e6 + 400
+       
