67ca62042349a1ca9706c9841ec7081afdc3ac9f
mathooo <trojak@mail.muni.cz>
2018-10-03 13:01:20 +0200

    Get_output: added ssh connection

--- libs/bioreactor.py
+++ libs/bioreactor.py
@@ -1,15 +1,33 @@
 import subprocess
 import time
+from paramiko import SSHClient
+from scp import SCPClient
 
 GET = ['gosh', 'scm_scripts/get_values.scm']
 SET = ['gosh', 'scm_scripts/set_values.scm']
+USER = 'root'
+FOLDER = '/root/control/'
+SERVER = '192.168.17.13'
 
 def get_output():
     '''
     Reads output from bioreactor by calling a Scheme script
     '''
-    result = subprocess.run(['gosh', 'scm_scripts/get_values.scm'], stdout=subprocess.PIPE)
-    return eval(result.stdout.decode('utf-8'))
+    ssh = SSHClient() 
+    ssh.load_system_host_keys()
+    ssh.connect(SERVER, username=USER)
+
+    sftp.put(file, FOLDER + os.path.basename(GET[1]))
+
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("gosh " + FOLDER + re.escape(os.path.basename(file)))
+
+    print(ssh_stdin, ssh_stdout, ssh_stderr)
+    
+    sftp.close()
+    ssh.close()
+
+    return #eval(result.stdout.decode('utf-8'))
+
 
 def set_input(value, input):
     '''
