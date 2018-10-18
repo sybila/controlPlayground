2b615624d9d2a9afb176674d4b39ddea8dfbebdf
mathooo <trojak@mail.muni.cz>
2018-10-18 09:36:39 +0200

    Updated connect script

--- admin/connect.sh
+++ admin/connect.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
-sudo openvpn --config ~/Documents/config/GCRI-DoAB.ovpn
+# use in separate terminals
+sudo umount -l /mnt/drasov_server
 sudo sshfs -o allow_other,IdentityFile=~/.ssh/id_rsa trojak.m@192.168.17.200:/home/trojak.m/ /mnt/drasov_server/
+
+#
+sudo openvpn --config ~/Documents/config/GCRI-DoAB.ovpn
