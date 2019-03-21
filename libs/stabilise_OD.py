df79479077248e6a0d532112e3052069cc6ef1b5
mathooo <trojak@mail.muni.cz>
2019-03-21 15:03:07 +0100

    Started implementing core function for stabilisation of OD

--- libs/stabilise_OD.py
+++ libs/stabilise_OD.py
@@ -1,18 +1,35 @@
 from scipy.optimize import curve_fit
 import numpy as np
+time
 
 OD_MAX = 0.525
 OD_MIN = 0.475
+TIMEOUT = 10
 
 # turns on pump and measured OD in cycle intil it reaches OD_MIN (with some tolerance)
-def pump_out_population(OD_MIN):
-	pass
+def pump_out_population(device, OD_MIN, pump, TIMEOUT):
+	device.set_pump_state(pump, True)
+	OD = measure_value(device)
+	while OD > OD_MIN: # or make a better condition with some tolerance
+		time.wait(TIMEOUT)
+		OD = measure_value(device)
+	device.set_pump_state(pump, False)
+	return True
 
 # given a PBR, it checks in cycle the OD every n seconds, once OD_MAX is reached it
 # calculates current growth rate using measured OD data and exponentional regression
 # has to remember initial OD!
-def reach_max_population(OD_MAX):
-	pass
+def reach_max_population(device, OD_MIN, OD_MAX, TIMEOUT):
+	values = []
+	OD = measure_value(device)
+	initial = float(OD)
+	while OD < OD_MAX: # or make a better condition with some tolerance
+		time.wait(TIMEOUT)
+		if OD > OD_MIN:
+			values.append(OD)
+		OD = measure_value(device)
+	time = np.linspace(1, len(values), 1) # could be actually measured as real time
+	return exponentional_regression(times, values, initial)
 
 # once max population is computed, we obtain current growth rate and this function
 # will use it and K last values to compute linear regression and decide whether
@@ -42,8 +59,8 @@ def main(node, conditions):
 	rate = None
 	#should return True if not stabilised
 	while growth_checker(rate):
-		rate = reach_max_population(OD_MAX)
-		pump_out_population(OD_MIN)
+		rate = reach_max_population(node.PBR, OD_MIN, OD_MAX, TIMEOUT)
+		pump_out_population(node.PBR, OD_MIN, pump, TIMEOUT)
 	return rate #which should be stabile
 
 if __name__ == '__main__':
