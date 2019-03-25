8076c9f5663509b62e0d8c72b59e52ed336aeafa
mathooo <trojak@mail.muni.cz>
2019-03-25 13:45:34 +0100

    Added DataHolder for better OD manipulation and measurement

--- libs/stabilise_OD.py
+++ libs/stabilise_OD.py
@@ -6,30 +6,22 @@ OD_MAX = 0.525
 OD_MIN = 0.475
 TIMEOUT = 10
 
-# turns on pump and measured OD in cycle intil it reaches OD_MIN (with some tolerance)
-def pump_out_population(device, OD_MIN, pump, TIMEOUT):
-	device.set_pump_state(pump, True)
-	OD = measure_value(device)
-	while OD > OD_MIN: # or make a better condition with some tolerance
-		time.wait(TIMEOUT)
-		OD = measure_value(device)
-	device.set_pump_state(pump, False)
-	return True
+# it holds all measured ODs
+# also is responsible for measurements and for calculation of median
+# from last several measurements (due to possible errors in measurement)
+class DataHolder():
+	def __init__(self, device):
+		self.device = device
+		self.data = []
 
-# given a PBR, it checks in cycle the OD every n seconds, once OD_MAX is reached it
-# calculates current growth rate using measured OD data and exponentional regression
-# has to remember initial OD!
-def reach_max_population(device, OD_MIN, OD_MAX, TIMEOUT):
-	values = []
-	OD = measure_value(device)
-	initial = float(OD)
-	while OD < OD_MAX: # or make a better condition with some tolerance
-		time.wait(TIMEOUT)
-		if OD > OD_MIN:
-			values.append(OD)
-		OD = measure_value(device)
-	times = np.linspace(1, len(values), len(values)) # could be actually measured as real time
-	return exponentional_regression(times, values, initial)
+	def measure_value(self):
+		self.data.append(self.device.measure_od())
+
+	def get_median(self, t):
+		for _ in range(n):
+			self.measure_value()
+			time.wait(1)
+		return np.median(self.data[-t:])
 
 # once max population is computed, we obtain current growth rate and this function
 # will use it and K last values to compute linear regression and decide whether
@@ -47,9 +39,23 @@ class GrowthChecker():
 			return coeff > abs(self.tolerance)
 		return True
 
-# measure current OD
-def measure_value(device):
-	return device.measure_od()
+# turns on pump and measured OD in cycle intil it reaches OD_MIN (with some tolerance)
+def pump_out_population(holder, OD_MIN, pump, TIMEOUT):
+	holder.device.set_pump_state(pump, True)
+	while holder.get_median(5) > OD_MIN: # or make a better condition with some tolerance
+		time.wait(TIMEOUT)
+	holder.device.set_pump_state(pump, False)
+	return True
+
+# given a PBR, it checks in cycle the OD every n seconds, once OD_MAX is reached it
+# calculates current growth rate using measured OD data and exponentional regression
+# has to remember initial OD!
+def reach_max_population(holder, OD_MIN, OD_MAX, TIMEOUT):
+	initial = len(holder.data)
+	while holder.get_median(5) < OD_MAX: # or make a better condition with some tolerance
+		time.wait(TIMEOUT)
+	times = np.linspace(1, len(holder.data), len(holder.data)) # could be actually measured as real time
+	return exponentional_regression(times, holder.data, holder.data[initial])
 
 # it is called when we start with new conditions
 # and they are all set for given node
@@ -70,15 +76,16 @@ def linear_regression(t, y):
 	popt, pcov = curve_fit(lambda t, a, b: a * t + b, times, values)
 	return popt[0]
 
-# this should do a particle
+# this is a function called by a particle, provides growth rate for given conditions
 def get_growth_rate(node, conditions):
 	# set initial conditions
 	set_up_conditions(node, conditions)
 	checker = GrowthChecker()
+	holder = DataHolder(node.PBR)
 	#should return True if not stabilised
 	while checker.is_stable(6):
-		checker.values.append(reach_max_population(node.PBR, OD_MIN, OD_MAX, TIMEOUT))
-		pump_out_population(node.PBR, OD_MIN, pump, TIMEOUT)
+		checker.values.append(reach_max_population(holder, OD_MIN, OD_MAX, TIMEOUT))
+		pump_out_population(holder, OD_MIN, pump, TIMEOUT)
 	return checker.values[-1] #which should be stabile
 
 if __name__ == '__main__':
