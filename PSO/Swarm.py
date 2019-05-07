064abf3801475a12f4fe76d6dab3660ed1068326
mathooo <trojak@mail.muni.cz>
2019-05-07 10:48:30 +0200

    Experiment results are saving to csv and png, clean up of main

--- PSO/Swarm.py
+++ PSO/Swarm.py
@@ -2,19 +2,26 @@ import threading
 import numpy as np
 import bioreactor
 
+import matplotlib.pyplot as plt
+import csv
+
+COLOURS = "bgrcmyk"
+
 # an Observer over all threads globally
 # workers append their results to shared list
 # observer takes them one by one and evaluates them
 class Swarm(threading.Thread):
-	def __init__(self, results, multiparametric_space, dir_name, optimum_type=-1):
+	def __init__(self, multiparametric_space, dir_name, optimum_type=-1):
 		super(Swarm, self).__init__()
 
 		self.optimum_type = optimum_type # optimum type, -1 for min
-		self.swarm_results = results 
+		self.swarm_results = [] 
 		self.boundaries = self.create_boundaries(multiparametric_space)
 		self.No_of_results = 0
 		self.stoprequest = threading.Event()
 		self.log = open(dir_name + "/history.log", "a")
+		self.dir_name = dir_name
+		self.particles = []
 
 	def run(self):
 		while not self.stoprequest.isSet():
@@ -23,13 +30,14 @@ class Swarm(threading.Thread):
 				if new_value[1]*self.optimum_type > self.swarm_best[1]*self.optimum_type:
 					self.swarm_best = new_value
 				self.condition_holds()
+		self.exit()
 
 	def condition_holds(self):
 		self.log.write(bioreactor.show_time() + "Swarm best so far: No " +
 					str(self.No_of_results) + ", best: " + str(self.swarm_best) + "\n")
 		self.log.flush()
 		self.No_of_results += 1
-		if self.No_of_results > 100:
+		if self.No_of_results > 20:
 			self.stoprequest.set()
 
 	def join(self, timeout=None):
@@ -49,9 +57,6 @@ class Swarm(threading.Thread):
 		self.swarm_best = (np.array(self.swarm_best), - self.optimum_type * np.inf)
 		return boundaries
 
-	def set_particles(self, particles):
-		self.particles = particles
-
 	def add_particle(self, particle):
 		particle.start()
 		self.particles.append(particle)
@@ -65,5 +70,50 @@ class Swarm(threading.Thread):
 			particle.exit()
 		self.stoprequest.set()
 
-def column(i, space):
-	return np.array([row[i] for row in space])
+	def save(self):
+		self.rows = []
+		self.header = list(self.parameter_keys)
+		n_params = len(self.parameter_keys)
+		n_particles = len(self.particles)
+		for i, particle in enumerate(self.particles):
+			self.header.append(particle.node.PBR.ID)
+			for point in particle.particle_trace:
+				row = [None] * (n_particles + n_params)
+				row[0:n_params] = point[0]
+				row[i+n_params] = point[1]
+				self.rows.append(row)
+		self.save_csv()
+		self.save_plot()
+
+	def save_csv(self):
+		with open(self.dir_name + '/results.csv', mode='w') as file:
+			row_writer = csv.writer(file, delimiter=',')
+			row_writer.writerow(self.header)
+			for row in self.rows:
+				row_writer.writerow(row)
+
+	def save_plot(self):
+		f, ax = plt.subplots()
+
+		for i, particle in enumerate(self.particles):
+			temps = temp(particle.particle_trace)
+			ODs = OD(particle.particle_trace)
+			ax.plot(temps, ODs, '-' + COLOURS[i], label=particle.node.PBR.ID)
+			ax.plot(temps[0], ODs[0], '>' + COLOURS[i], label=particle.node.PBR.ID + ' start')
+			ax.plot(temps[-1], ODs[-1], '*'  + COLOURS[i], label=particle.node.PBR.ID + ' end')
+
+		ymin, ymax = ax.get_ylim()
+		ax.set_yticks(np.round(np.linspace(ymin, ymax, 10), 2))
+
+		xmin, xmax = ax.get_xlim()
+		ax.set_xticks(np.round(np.linspace(xmin, xmax, 10), 2))
+
+		legend = ax.legend(shadow=True)
+
+		plt.savefig(self.dir_name + "/results.png", dpi=150)
+
+def temp(data):
+	return [d[0][0] for d in data]
+
+def OD(data):
+	return [d[1] for d in data]
--- PSO/main.py
+++ PSO/main.py
@@ -9,113 +9,90 @@ from Swarm import *
 import bioreactor
 from prompt import prompt
 
-now =  datetime.datetime.now() + datetime.timedelta(hours=2)
-# dir_name = ".log/" + '{:%Y%m%d-%H%M%S}'.format(now)
-dir_name =  ".log/TESTING" # for testing
-os.mkdir(dir_name)
+params = ["temp", "light-red", "light-blue", "flow"]
 
-# redirect = open(dir_name + '/history.log', 'a')
-# sys.stderr = redirect
-# sys.stdout = redirect
+print("Initial setup...")
+
+now =  datetime.datetime.now() + datetime.timedelta(hours=2)
+dir_name = ".log/" + '{:%Y%m%d-%H%M%S}'.format(now)
+# dir_name =  ".log/TESTING" # for testing
+os.mkdir(dir_name)
 
 node_IDs = ["PBR02", "PBR03", "PBR07"]
 
 for ID in node_IDs:
 	os.mkdir(dir_name + "/" + ID)
 
-params = ["temp", "light-red", "light-blue", "flow"]
+####### setup nodes ########
 
-print("Initial setup...")
-sys.stdout.flush()
+nodes = []
 
-#node1 = bioreactor.Node(1)
-#node1.add_device("PBR", "PBR01", 72700001)
-# node.add_device("GMS", "GMS", 46700003)
-# node.add_device("GAS", "GAS", 42700007)
+nodes.append(bioreactor.Node(2))
+nodes[-1].add_device("PBR", "PBR02", 72700002)
+nodes[-1].setup_stabiliser(dir_name)
 
-node2 = bioreactor.Node(2)
-node2.add_device("PBR", "PBR02", 72700002)
-node2.setup_stabiliser(dir_name)
+nodes.append(bioreactor.Node(3))
+nodes[-1].add_device("PBR", "PBR03", 72700003)
+nodes[-1].setup_stabiliser(dir_name)
 
-node3 = bioreactor.Node(3)
-node3.add_device("PBR", "PBR03", 72700003)
-node3.setup_stabiliser(dir_name)
-
-node7 = bioreactor.Node(7)
-node7.add_device("PBR", "PBR07", 72700007)
-node7.setup_stabiliser(dir_name)
+nodes.append(bioreactor.Node(7))
+nodes[-1].add_device("PBR", "PBR07", 72700007)
+nodes[-1].setup_stabiliser(dir_name)
 
 print("Devices ready.")
-sys.stdout.flush()
 
 ############ initial setup #############
-node2.PBR.set_pwm(50, True)
-node2.PBR.turn_on_light(0, True)
-node2.PBR.turn_on_light(1, True)
-node2.PBR.set_pump_state(5, False)
-node2.PBR.set_light_intensity(0, 200)
-node2.PBR.set_light_intensity(1, 150)
 
-node3.PBR.set_pwm(50, True)
-node3.PBR.turn_on_light(0, True)
-node3.PBR.turn_on_light(1, True)
-node3.PBR.set_pump_state(5, False)
-node3.PBR.set_light_intensity(0, 200)
-node3.PBR.set_light_intensity(1, 150)
+nodes[-1].PBR.set_pwm(50, True)
+nodes[-1].PBR.turn_on_light(0, True)
+nodes[-1].PBR.turn_on_light(1, True)
+nodes[-1].PBR.set_pump_state(5, False)
+nodes[-1].PBR.set_light_intensity(0, 200)
+nodes[-1].PBR.set_light_intensity(1, 150)
 
-node7.PBR.set_pwm(50, True)
-node7.PBR.turn_on_light(0, True)
-node7.PBR.turn_on_light(1, True)
-node7.PBR.set_pump_state(5, False)
-node7.PBR.set_light_intensity(0, 200)
-node7.PBR.set_light_intensity(1, 150)
+nodes[-1].PBR.set_pwm(50, True)
+nodes[-1].PBR.turn_on_light(0, True)
+nodes[-1].PBR.turn_on_light(1, True)
+nodes[-1].PBR.set_pump_state(5, False)
+nodes[-1].PBR.set_light_intensity(0, 200)
+nodes[-1].PBR.set_light_intensity(1, 150)
+
+nodes[-1].PBR.set_pwm(50, True)
+nodes[-1].PBR.turn_on_light(0, True)
+nodes[-1].PBR.turn_on_light(1, True)
+nodes[-1].PBR.set_pump_state(5, False)
+nodes[-1].PBR.set_light_intensity(0, 200)
+nodes[-1].PBR.set_light_intensity(1, 150)
+
 ########################################
 
 print("Setup done.")
-sys.stdout.flush()
-
-#nodes = [node1, node2, node3]
-nodes = [node2, node3, node7]
 
 # multiparametric_space = {params[1]: (100, 800), # red light
 # 						 params[2]: (100, 800)} # blue light
 
 multiparametric_space = {params[0]: (15, 35)}
 
-particles = []
-swarm_results = []  # important variable shared by all particles (including swarm)
+print("Creating and starting swarm...")
 
-swarm = Swarm(swarm_results, multiparametric_space, dir_name)
+swarm = Swarm(multiparametric_space, dir_name)
 swarm.type = -1
+swarm.start()
 
 # conditions = [np.array([561, 563]), np.array([211, 164]), np.array([327, 404])]
-# conditions = [np.array([211, 164]), np.array([327, 404])]
 conditions = [np.array([21]), np.array([25]), np.array([29])]
 
-n_of_nodes = len(nodes)
-
-for i in range(n_of_nodes):
-	random_position = []
+for i in range(len(nodes)):
 	step = random.uniform(0, 1)
-	##### JUST TOO KEEP SAME CONDITIONS #######
-	# random_position = [200, 232]
+	##### choose random position #######
+	# random_position = []
 	# for key in swarm.parameter_keys:
 		# random_position.append(random.uniform(min(multiparametric_space[key]), max(multiparametric_space[key])))
 	################################
-	particles.append(Particle(conditions[i], step, swarm, nodes[i], dir_name))
-
-print("Swarm created, starting...")
-sys.stdout.flush()
-
-swarm.start()
-swarm.set_particles(particles)
-
-for particle in swarm.particles:
+	swarm.add_particle(Particle(conditions[i], step, swarm, nodes[i], dir_name))
 	time.sleep(5)
-	particle.start()
 
-print("Particles started")
-sys.stdout.flush()
+print("Swarm started.")
 
 while swarm.is_alive():
 	user_input = prompt.command(globals())
@@ -125,21 +102,7 @@ while swarm.is_alive():
 	except Exception as e:
 		print(e)
 
-print("************* Experiment is finishing ***********")
-
-for particle in swarm.particles:
-	particle.stoprequest.set()
-
-print("\n++++++++++++ OVERALL RESULTS ++++++++++++\n")
-
-values = []
-for particle in swarm.particles:
-	print("------- Results for particle", particle.node.PBR.ID, particle.name)
-	print(particle.particle_trace)
-	values.append(max(column(1, particle.particle_trace)))
-	print("BEST:", values[-1])
-
-print("Actual best:", max(values))
+print("Experiment finished.")
 print('Swarm best:', swarm.swarm_best)
 
-redirect.close()
+swarm.save()
